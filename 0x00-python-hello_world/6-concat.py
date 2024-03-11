2d23a4072d9245e9996413adc17b334de17efbd0
dominic6809 <muuomusyoki018@gmail.com>
2024-03-11 06:38:24 -0700

    print Welcome to Holberton School!

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-string1 = "Holberton"
-string2 = "School"
-string3 = string1 + " " + string2
-print("Welcome to " + string3 + "!")
+str1 = "Holberton"
+str2 = "School"
+str3 = str1 + " " + str2
+print(f"Welcome to {str3}!")
