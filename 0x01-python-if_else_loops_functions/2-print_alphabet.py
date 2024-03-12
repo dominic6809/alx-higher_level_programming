afae2c96e52fd011eb59749c7939c60bec7c80e5
dominic6809 <muuomusyoki018@gmail.com>
2024-03-12 00:31:02 -0700

    program that prints the ASCII alphabet, in lowercase, not followed by \n

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for i in range(97, 123):
+    print("{character}".format(character=chr(i)), end="")
+# Iterate over ASCII values from 97 to 122 (lowercase letters range)
+# and print each character without newline
