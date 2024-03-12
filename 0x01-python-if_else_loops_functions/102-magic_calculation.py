01207c04adac4ef42f8d544ec2caab1895304a8b
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-12 14:39:20 +0300

    Python function def magic_calculation(a, b, c)

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b, c):
-    if (a > c):
+    if (c > b):
         return (a + b)
     elif (a < b):
-        return c
+        return (c)
     else:
         return (a * b - c)
