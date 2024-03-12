890470d7005138e88bd1e7977150cfdd7c872dca
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-12 13:33:41 +0300

    a function that adds two integers and returns the result.

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -3,12 +3,3 @@
 def add(a, b):
     # Add the two integers and return the result
     return (a + b)
-
-# Test cases
-
-
-result = add(1, 2)
-print(result, end="")  # Print result with two chars without a newline
-
-result = add(98, 0)
-print(result, end="")  # Print result with two chars without a newline
