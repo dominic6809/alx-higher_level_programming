0de9db505e91eb61cb7a667bc0816813be5698cb
dominic6809 <muuomusyoki018@gmail.com>
2024-03-12 01:14:03 -0700

    a function that prints the last digit of a number

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    # Get the absolute value of the number to handle negative numbers
+    number = abs(number)
+    # Extract the last digit using the modulo operator
+    last_digit = number % 10
+    # Print the last digit
+    print(last_digit)
+    # Return the last digit
+    return last_digit
+
+# Test cases
+
+
+print_last_digit(123)
+
+print_last_digit(-456)
