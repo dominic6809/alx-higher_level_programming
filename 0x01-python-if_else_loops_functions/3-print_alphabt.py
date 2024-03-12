c53224560da719ccc201f560e618c94589b5be94
dominic6809 <muuomusyoki018@gmail.com>
2024-03-12 02:41:12 -0700

    a program that prints the ASCII alphabet, in lowercase, not followed by \n

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-# Loop through ASCII values for lowercase letters
 for i in range(97, 123):
-    # Skip printing 'e' and 'q'
-    if i != 101 and i != 113:
-        # Print the character
-        print("{:c}".format(i), end="")
+    # skip printing 'e' and 'q'
+    if (i != 101 and i != 113):
+        # print the character
+        print('{:c}'.format(i), end='')
