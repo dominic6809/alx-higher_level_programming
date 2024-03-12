1043c79ddfd6a57e4efb419b28c46a6e457f0fa7
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-12 13:50:53 +0300

    a function that prints the numbers from 1 to 100 separated by a space

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    # Initialize a counter for the number of characters printed
-    char_count = 0
-    
-    # Iterate through numbers from 1 to 100
-    for i in range(1, 101):
-        # Check if the number is a multiple of both 3 and 5
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
-            char_count += 8  # Increment char count by len of "FizzBuzz" + a space
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-            char_count += 5  # Increment char count by len of "Fizz" + a space
-        # Check if the number is a multiple of 5 only
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-            char_count += 5  # Increment char count by len of "Buzz" + a space
-        # If the number is not a multiple of 3 or 5, print the number itself
+    """
+    Print FizzBuzz game output for numbers from 1 to 100.
+    For multiples of 3, print "Fizz".
+    For multiples of 5, print "Buzz".
+    For multiples of both 3 and 5, print "FizzBuzz".
+    Otherwise, print the number itself.
+    """
+    for n in range(1, 101):
+        if n % 3 == 0 and n % 5 == 0:
+            print('FizzBuzz', end=' ')
+        elif n % 3 == 0:
+            print('Fizz', end=' ')
+        elif n % 5 == 0:
+            print('Buzz', end=' ')
         else:
-            num_str = str(i)
-            print(num_str, end=" ")
-            char_count += len(num_str) + 1
-        
-        # Check if total characters printed reaches 415
-        if char_count >= 415:
-            break
+            print(i, end=' ')
 
 # Test the function
 
