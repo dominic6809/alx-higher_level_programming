638e921dcedbd950677bab92665bfdbcc0afac2e
dominic6809 <muuomusyoki018@gmail.com>
2024-03-12 02:30:37 -0700

    program will assign a random signed number to the variable number each time it is executed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,16 +4,22 @@ import random
 # Generate a random signed number between -10000 and 10000
 number = random.randint(-10000, 10000)
 
-# Get the last digit of the absolute value of the number
-last_digit = abs(number) % 10
-
-# Print the message along with the last digit of the number
-print(f'The string Last digit of {number} is {last_digit}', end=' ')
-
-# Check the value of the last digit and print the appropriate message
-if last_digit > 5:
-    print('and is greater than 5')
-elif last_digit == 0:
-    print('and is 0')
+# Determine the last digit of the number
+if number == 0:
+    last_digit = 0
+elif number > 0:
+    last_digit = int(str(number)[-1])  # Convert last char of number to integer
 else:
-    print('and is less than 6 and not 0')
+    last_digit = -1 * (int(str(number)[-1]))
+    # Convert last char of number to int and negate it
+
+# Determine the suffix based on the value of the last digit
+if last_digit > 5:
+    suffix = "and is greater than 5"
+elif last_digit == 0:
+    suffix = "and is 0"
+elif last_digit < 6 and last_digit != 0:
+    suffix = "and is less than 6 and not 0"
+
+# Print the result with appropriate formatting
+print(f"Last digit of {number:d} is {last_digit:d} {suffix}")
