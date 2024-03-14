cac8ff108b947dcba8848e7d0760c98878440318
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-14 16:11:35 +0300

    program that imports functions from the file calculator_1.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-
-# Import functions from calculator_1 module
 from calculator_1 import add, sub, mul, div
-
-# Check if this script is run directly
 if __name__ == "__main__":
-    # Define variables 'a' and 'b'
     a = 10
     b = 5
-    # Perform arithmetic operations and print results
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
