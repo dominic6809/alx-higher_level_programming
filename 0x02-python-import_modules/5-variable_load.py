cd971280eb71811f7964d855f48c93b22d9cd675
dominic6809 <muuomusyoki018@gmail.com>
2024-03-14 06:05:19 -0700

    a program that imports the variable a from the file variable_load_5.py and prints its value.

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Import variable 'a' from module variable_load_5
+from variable_load_5 import a
+
+# Check if this script is run directly
+if __name__ == "__main__":
+    # Print the value of variable 'a'
+    print("{}".format(a))
