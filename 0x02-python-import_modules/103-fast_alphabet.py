7c0d6a635f738d385e08d5aac5c7c27fb83f8c02
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-14 16:43:15 +0300

    a program that prints the alphabet in uppercase, followed by a new line.

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-# Import the string module
 import string
-
-# Print the uppercase ASCII letters
-print(string.ascii_uppercase)
+print(string.ascii_uppercase)  # uppercase ASCII letters
