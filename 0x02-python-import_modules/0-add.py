edec01cb0f7af4033bb511c5f713a662efe9d711
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-14 14:03:07 +0300

    program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-# Assigning the add function from add_0 module to the local namespace
 from add_0 import add
-# Checking if the script is executed directly
 if __name__ == "__main__":
+    """
+    Assigning the add function from add_0 module to the local namespace
+    Checking if the script is executed directly
+    Prints the result of two added numbers
+    """
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
