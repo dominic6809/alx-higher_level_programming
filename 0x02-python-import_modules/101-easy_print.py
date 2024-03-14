ef9d7023a44f72e7e43ac44b96d37d79cc90a5c2
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-14 16:34:32 +0300

    a program that prints #pythoniscool, followed by \n

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -2,5 +2,4 @@
 
 # Import the 'os' module and call the write method to write a string to stdout
 # The string "#pythoniscool\n" is encoded into bytes before writing
-import os
-os.write(1, "#pythoniscool\n".encode())
+__import__('os').write(1, "#pythoniscool\n".encode())
