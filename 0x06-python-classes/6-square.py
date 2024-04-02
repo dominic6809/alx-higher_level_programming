e5e17508a0ec4226386e09e1ab1b80f75f72ecbc
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-02 16:19:59 +0300

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,1 +1,81 @@
 #!/usr/bin/python3
+"""
+Defines class Square
+"""
+
+
+class Square:
+    """Constructor for a square class"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        params:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+       """
+        Getter method for size attribute.
+
+        Returns:
+            int: Size of the square.
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+      """
+        Setter method for size attribute.
+
+        parameters:
+            value (int): Size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+      """
+        Getter method for position attribute.
+
+        Returns:
+            tuple: Position of the square.
+        """
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """
+        Print the square with the character '#' to stdout.
+        """
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for t in range(0, self.__position[1])]
+        for t in range(0, self.__size):
+            [print(" ", end="") for s in range(0, self.__position[0])]
+            [print("#", end="") for u in range(0, self.__size)]
+            print("")
