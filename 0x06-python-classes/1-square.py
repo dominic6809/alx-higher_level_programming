d84e5a9433ba006fcd9bca831e7e052773bc6fb3
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-02 15:30:13 +0300

    class Square that defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+A class Square
+that defines a square based on 0-square.py
+"""
+
+
+class Square:
+    def __init__(self, size):
+        """
+        Constructor for Square class.
+
+        Parameters:
+        size (int): Size of the square.
+        """
+        self.__size = size
+       """
+        Private instance attribute: size
+        Instantiation with size
+        (no type/value verification)
+        """
