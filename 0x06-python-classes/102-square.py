6f4795c430c6b90903389f1c0e06292a6b5c08d0
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-02 19:31:42 +0300

    compares two squares

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -3,6 +3,10 @@
 
 
 class Square:
+    """square
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size=0):
         """initializes the square
         params:
@@ -13,33 +17,27 @@ class Square:
         self.size = size
 
     def area(self):
-        """compute the square's area
+        """computes the square's area
         Returns:
-            number: Area of the square
+            The area of the square
         """
         return (self.__size) ** 2
 
     @property
     def size(self):
-        """
-        Retrieves the size of the square.
-
+        """Retrieves the size of the square
         Returns:
-            number: Size of the square.
+            The size of the square
         """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        Sets the size of the square.
-
+        """Sets the size of the square
         params:
-            value (number): Size value to set.
-
-        Raises:
-            TypeError: If value is not a number.
-            ValueError: If value is less than 0.
+            value (int): the size of a size of the square
+        Returns:
+            None
         """
         if type(value) is not int:
             raise TypeError("size must be an integer")
@@ -50,73 +48,55 @@ class Square:
                 self.__size = value
 
     def __lt__(self, other):
-        """
-        Less than comparator.
-
+        """Less than comparator
         params:
-            other (Square): Another Square object.
-
+            other (Square): square to compare against
         Returns:
-            bool: True if area is less, False otherwise.
+            True or False
         """
         return self.size < other.size
 
     def __le__(self, other):
-        """
-        Less than or equal comparator.
-
+        """Less than or equal comparator
         params:
-            other (Square): Another Square object.
-
+            other (Square): square to compare against
         Returns:
-            bool: True if area is less than or equal, False otherwise.
+            True or False
         """
         return self.size <= other.size
 
     def __eq__(self, other):
-        """
-        Equal comparison.
-
+        """Equal comparator
         params:
-            other (Square): Another Square object.
-
+            other (Square): square to compare against
         Returns:
-            bool: True if areas are equal, False otherwise.
+            True or False
         """
         return self.size == other.size
 
     def __ne__(self, other):
-        """
-        Not equal comparison operator.
-
+        """Not equal comparator
         params:
-            other (Square): Another Square object.
-
+            other (Square): square to compare against
         Returns:
-            bool: True if areas are not equal, False otherwise.
+            True or False
         """
         return self.size != other.size
 
     def __ge__(self, other):
-        """
-        Greater than or equal comparison operator.
-
+        """Greater than or equal comparator
         params:
-            other (Square): Another Square object.
-
+            other (Square): square to compare against
         Returns:
-            bool: True if area is greater than or equal, False otherwise.
+            bool: True if area is greater than or equal, False otherwise
         """
         return self.size >= other.size
 
     def __gt__(self, other):
-        """
-        Greater than comparator.
-
+        """Greater than comparator
         params:
-            other (Square): Another Square object.
-
+            other (Square): square to compare against
         Returns:
-            bool: True if area is greater, False otherwise.
+            bool: True if area is greater than or equal, False otherwise
         """
         return self.size > other.size
