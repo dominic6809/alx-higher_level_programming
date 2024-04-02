d02f50f9a8014dd9451202a23fa62d04615759ce
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-02 13:08:34 +0300

    function that prints an integer with "{:d}".format()

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def safe_print_integer(value):
+    """
+    Print an integer with "{:d}".format().
+
+    :param value: Value to print (can be any type)
+    :return: True if value has been correctly printed as an integer, False otherwise
+    """
+    try:
+        print("{:d}".format(value))
+        print()
+        return (True)
+    except (ValueError, TypeError):
+        return (False)
