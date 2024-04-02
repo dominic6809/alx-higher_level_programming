251eed53a1001d22810f1feb36b95c7ea3a2ee9a
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-02 14:09:59 +0300

    ByteCode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+def magic_calculation(a, b):
+    """
+    Perform a magic calculation.
+
+    :param a: Integer
+    :param b: Integer
+    :return: Result of the magic calculation
+    """
+  result = 0
+    for num in range(1, 3):
+        try:
+            if num > a:
+                raise Exception('Too far')
+            result += a ** b / num
+        except Exception:
+            result = b + a
+            break
+    return result
