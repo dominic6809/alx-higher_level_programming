67243f28413b045c7ab03c859c2f6b43aa5d3605
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-02 15:42:03 +0300

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""
-Defines a square class
-"""
+
+"""Define class Square."""
 
 
 class Square:
-   """
-        Constructor for Square class.
-        """
+    """constructor for a square."""
+
     def __init__(self, size=0):
+        """Initialize a new square.
+
+        params:
+            size (int): size of the new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
+
+    def area(self):
+        """Return current area of the square."""
+        return self.__size * self.__size
