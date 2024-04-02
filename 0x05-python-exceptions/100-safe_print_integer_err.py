b761272453d50acf694007ae47aed8705681c792
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-02 13:51:57 +0300

    function that prints an integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """
+    Print an int and return True if successful, otherwise, False
+    and print an error message to stderr.
+    :param value: Value to print
+    :return: True if value has correctly printed as an int, False otherwise
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError) as error:
+        print("Exception:", error, file=sys.stderr)
+        return False
