72e68130bd016b53931433dc08670d352ec305e6
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-02 13:38:13 +0300

    a function that raises a type exception.

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def raise_exception():
+    """
+    Raises a type exception.
+
+    """
+    try:
+        '{}'.format(42)
+    except TypeError as e:
+        raise e
+    finally:
+        pass
