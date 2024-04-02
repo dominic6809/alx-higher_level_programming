685831bd9735b1d3901cec518326e7711e669ceb
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-02 13:47:05 +0300

    function that raises a name exception with a message

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -2,8 +2,6 @@
 def raise_exception_msg(message=""):
     """
     Raises a name exception with a message.
-
     :param message: The message to be included in the exception
     """
-  def raise_exception_msg(message=""):
     raise NameError(message)
