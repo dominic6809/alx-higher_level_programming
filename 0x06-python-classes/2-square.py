93b630372d47c0a3bb164192d77767f12f3aa0fa
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-02 15:38:29 +0300

    class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+Defines a square class
+"""
+
+
+class Square:
+   """
+        Constructor for Square class.
+        """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
