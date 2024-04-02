e6a1e0de8249a490b00192876ca0889da2b0c652
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-02 16:20:51 +0300

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -74,8 +74,8 @@ class Square:
             print("")
             return
 
-        [print("") for t in range(0, self.__position[1])]
-        for t in range(0, self.__size):
-            [print(" ", end="") for s in range(0, self.__position[0])]
-            [print("#", end="") for u in range(0, self.__size)]
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             print("")
