ced30366a324c52140f698817b69c26a6311b8a0
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-09 11:31:13 +0300

    integer addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python3
+"""
+ function that adds 2 integers.
+ """
 
+
+def add_integer(a, b=98):
+    """
+    Returns:
+        int: Sum of a and b.
+
+    params:
+        a (int or float): First integer or float.
+        b (int or float): Second integer or float. Defaults to 98.
+        
+    Raises:
+        TypeError: If a or b is not an integer or float.
+    """
+    if not(isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+
+    return int(a + b)
