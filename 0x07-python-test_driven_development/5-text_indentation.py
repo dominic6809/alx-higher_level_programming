21bbf6f33681fdc30fd7988e414796da855ca3ab
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-09 13:11:42 +0300

    text-identation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python3
+"""
+text-identation module
+"""
 
+
+def text_indentation(text):
+    """
+    Print a text with new lines after each of ., ? and :.
+
+    params:
+        text (str): The text to process.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    # Check if text is a string
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    letter_result = ""
+    
+    for letter in text:
+        letter_result += letter
+        if letter in (".", "?", ":"):
+            letter_result += "\n\n"
+    
+    # Print the processed text
+    print(letter_result.rstrip())
