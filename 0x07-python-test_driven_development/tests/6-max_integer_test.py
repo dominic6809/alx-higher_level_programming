458071b8c9f7ba03bccf451770156fd22e5c0a37
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-11 11:47:22 +0300

    unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,43 +11,23 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """Test for max integer"""
 
-    def test_basic_check(self):
-        """Basic Check"""
-        self.assertEqual(max_integer([5, 30, 74]), 74)
+    def test_max_integer(self):
+        """
+        Test all possible occurences of max integers
+        """
+        self.assertIsNone(max_integer([]))
+        self.assertAlmostEqual(max_integer([2, 3, 4, 5, 6]), 6)
+        self.assertAlmostEqual(max_integer([-4, -3, -2, -1, 0]), 0)
+        self.assertAlmostEqual(max_integer([-90, -120, -150, -180]), -90)
+        self.assertAlmostEqual(max_integer([1.2, 1.5, 1.6, 5.7, 2.5]), 5.7)
+        self.assertAlmostEqual(max_integer([9.9]), 9.9)
 
-    def test_the_same_check(self):
-        """Test the same numbers"""
-        self.assertEqual(max_integer([5, 5, 5]), 5)
-
-    def test_begin_check(self):
-        """Test the beginning numbers"""
-        self.assertEqual(max_integer([180, 3, 7]), 180)
-
-    def test_middle_check(self):
-        """Test the middle numbers"""
-        self.assertEqual(max_integer([7, 63, 7]), 63)
-
-    def test_one_neg_check(self):
-        """Test one negative numbers"""
-        self.assertEqual(max_integer([6, 83, -33]), 83)
-
-    def test_neg_check(self):
-        """Test only negative numbers"""
-        self.assertEqual(max_integer([-6, -83, -33]), -6)
-
-    def test_one_check(self):
-        """Test only one number"""
-        self.assertEqual(max_integer([-5]), -5)
-
-    def test_empty_check(self):
-        """Test empty list"""
-        self.assertFalse(max_integer([]))
-
-    def test_non_integer_check(self):
-        """Test list with non integer elements"""
+    def test_wrong_types(self):
+        """
+        Test max integer with wrong parameters types
+        """
         with self.assertRaises(TypeError):
-            max_integer([23, "23", 233])
+            max_integer(None)
 
-
-if __name__ == '__main__':
-    unittest.main()
+        with self.assertRaises(TypeError):
+            max_integer(["Monty", 89, 34, -9.7, "Python"])
