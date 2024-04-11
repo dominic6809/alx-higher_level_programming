9cae6804051114fc4ecf2258522ddc5aba00a15a
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-11 09:48:10 +0300

    divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -16,32 +16,20 @@ def matrix_divided(matrix, div):
     Returns:
         list of lists: New matrix with elements divided by div
     """
-    new_matrix = []
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if (not isinstance(div, float) and not isinstance(div, int)):
-        raise TypeError("div must be a number")
-    if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix \
-                (list of lists) of integers/floats")
-    try:
-        size = len(matrix[0])
-    except Exception:
-        raise TypeError("matrix must be a matrix \
-                (list of lists) of integers/floats")
-    for i in matrix:
-        if not isinstance(i, list):
-            raise TypeError("matrix must be a matrix \
-                    (list of lists) of integers/floats")
-        if len(i) != size:
-            raise TypeError("Each row of the matrix must have the same size")
-        new_row = []
-        for number in i:
-            if not isinstance(number, int) and not isinstance(number, float):
-                raise TypeError("matrix must be a matrix \
-                        (list of lists) of integers/floats")
-            initial = float(number / div)
-            result = float("{:.2f}".format(initial))
-            new_row.append(result)
-        new_matrix.append(new_row)
-    return new_matrix
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
