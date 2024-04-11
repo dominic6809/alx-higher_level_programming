75242ac70169f5dac09e6bbc8fa4f5d83ca3d2fb
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-11 11:37:00 +0300

    unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -59,5 +59,6 @@ class TestMaxInteger(unittest.TestCase):
         """Test case for a list of nested lists."""
         self.assertEqual(max_integer([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), 9)
 
+
 if __name__ == '__main__':
     unittest.main()
