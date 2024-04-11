66ae99e8a07219d2e65a9fe5dcf2b6f630a980fd
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-11 12:14:45 +0300

    max-integer

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,30 +4,16 @@ unittest class module
 """
 
 
-import unittest
-max_integer = __import__('6-max_integer').max_integer
-
-
-class TestMaxInteger(unittest.TestCase):
-    """Test for max integer"""
-
-    def test_max_integer(self):
-        """
-        Test all possible occurences of max integers
-        """
-        self.assertIsNone(max_integer([]))
-        self.assertAlmostEqual(max_integer([2, 3, 4, 5, 6]), 6)
-        self.assertAlmostEqual(max_integer([-4, -3, -2, -1, 0]), 0)
-        self.assertAlmostEqual(max_integer([-90, -120, -150, -180]), -90)
-        self.assertAlmostEqual(max_integer([1.2, 1.5, 1.6, 5.7, 2.5]), 5.7)
-        self.assertAlmostEqual(max_integer([9.9]), 9.9)
-
-    def test_wrong_types(self):
-        """
-        Test max integer with wrong parameters types
-        """
-        with self.assertRaises(TypeError):
-            max_integer(None)
-
-        with self.assertRaises(TypeError):
-            max_integer(["Monty", 89, 34, -9.7, "Python"])
+def max_integer(list=[]):
+    """A function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+    if len(list) == 0:
+        return None
+    t_result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > t_result:
+            t_result = list[i]
+        i += 1
+    return t_result
