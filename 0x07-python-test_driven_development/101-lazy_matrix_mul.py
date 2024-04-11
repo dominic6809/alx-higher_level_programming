cf87a6bae4961ed0b45871993f6170f8e060e892
dominic6809 <muuomusyoki018@gmail.com>
2024-04-11 01:31:53 -0700

    lazy multiplication

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -41,7 +41,7 @@ def lazy_matrix_mul(m_a, m_b):
             if (not isinstance(val, int) and not isinstance(val, float)):
                 raise TypeError("m_a should contain only integers or floats")
         if len(m_a[0]) != len(row):
-                raise TypeError("each row of m_a must be of the same size")
+            raise TypeError("each row of m_a must be of the same size")
 
     # validate m_b
     for row in m_b:
