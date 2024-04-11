d187d31c6b84b2467615442b6a955e46f6f4e35a
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-11 11:30:17 +0300

    lazy multiplication

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -6,48 +6,54 @@ matrix multiply module using numpy
 
 def lazy_matrix_mul(m_a, m_b):
     """
-    function to Multiply two matrices using NumPy.
-
+    lazy_matrix_mul function
     params:
         m_a (list of lists): First matrix.
         m_b (list of lists): Second matrix.
-
     Returns:
-        numpy.ndarray: Result of matrix multiplication.
+        numpy.ndarray: Result of matrix multiplication"""
 
-    Raises:
-        TypeError: If either m_a or m_b is not a list of lists of ints/floats.
-        TypeError: If either m_a or m_b is not a rectangle.
-        TypeError: If either m_a or m_b contains non-integer/float elements
-        ValueError: If m_a or m_b is empty or can't be multiplied.
-    """
     import numpy as np
 
-    # Check if m_a and m_b are lists of lists
-    if not all(isinstance(row, list) for row in m_a) or
-    not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_a and m_b must be lists of lists")
+    """if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
 
-    # Check if m_a and m_b are non-empty
-    if not m_a or not m_b:
-        raise ValueError("m_a and m_b cannot be empty")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
 
     for row in m_a:
-        if not all(isinstance(elem, (int, float)) for elem in row):
-            raise TypeError("m_a should contain only integers or floats")
-        if len(row) != len(m_a[0]):
-            raise TypeError("each row of m_a must be of the same size")
+        if not isinstance(row, list):
+            raise TypeError("m_a must be a list of lists")
 
     for row in m_b:
-        if not all(isinstance(elem, (int, float)) for elem in row):
-            raise TypeError("m_b should contain only integers or floats")
-        if len(row) != len(m_b[0]):
-            raise TypeError("each row of m_b must be of the same size")
+        if not isinstance(row, list):
+            raise TypeError("m_b must be a list of lists")
 
-    # Check if number of columns in m_a matches number of rows in m_b
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b cannot be multiplied")
+    if (len(m_a) == 0 or len(m_a[0]) == 0):
+        raise ValueError("m_a can't be empty")
 
+    if (len(m_b) == 0 or len(m_b[0]) == 0):
+        raise ValueError("m_b can't be empty")"""
+
+    # validate m_a
+    for row in m_a:
+        for val in row:
+            if (not isinstance(val, int) and not isinstance(val, float)):
+                raise TypeError("m_a should contain only integers or floats")
+        if len(m_a[0]) != len(row):
+                raise TypeError("each row of m_a must be of the same size")
+
+    # validate m_b
+    for row in m_b:
+        for val in row:
+            if (not isinstance(val, int) and not isinstance(val, float)):
+                raise TypeError("m_b should contain only integers or floats")
+            if len(m_b[0]) != len(row):
+                raise TypeError("each row of m_b must be of the same size")
+
+    """if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+    """
     A = np.asmatrix(m_a)
     B = np.asmatrix(m_b)
     C = A.dot(B)
