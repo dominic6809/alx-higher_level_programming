6045fdfa0a059866abec95da768cfbb1cc5a0d5f
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-16 00:10:51 +0300

    class Square that inherits from Rectangle (9-rectangle.py):

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+function to Define a Rectangle subclass Square.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+        A class representing a square,
+        inheriting from Rectangle.
+        Attrib:
+            size (int): side of the square
+        Methods:
+            __init__ - initialises the square
+    """
+    def __init__(self, size):
+        """
+        Initialize a square with given size.
+
+        Args:
+        size (int): The size of the square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square."""
+        area = self.__size * self.__size
+        return area
+
+    def __str__(self):
+        return ("[{}] {}/{}".format("Rectangle",
+                                    self.__size, self.__size))
