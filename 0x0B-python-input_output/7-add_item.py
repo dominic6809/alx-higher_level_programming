d47195ba6674a6aa30ce1e1f265cb2d0a6cbf3f4
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-16 21:32:11 +0300

    load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,16 +5,19 @@ A python script that adds all arguments to a Python List.
 
 
 import sys
-
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+
+    filename = "add_item.json"
 
     try:
-        items = load_from_json_file("add_item.json")
+        my_list = load_from_json_file(filename)
     except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+        my_list = []
+
+    for item in sys.argv[1:]:
+        my_list.append(item)
+
+    save_to_json_file(my_list, filename)
