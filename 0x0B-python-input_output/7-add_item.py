066db22be0be3663abf1c64c76b23f0f33959ac8
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-16 22:32:24 +0300

    A python script that adds all arguments to a Python List.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,19 +2,20 @@
 """
 A python script that adds all arguments to a Python List.
 """
-
-
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 import sys
 
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-
+def main():
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
+
     items.extend(sys.argv[1:])
+
     save_to_json_file(items, "add_item.json")
+
+if __name__ == "__main__":
+    main()
