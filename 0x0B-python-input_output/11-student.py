4ac603986a1ca6322eefae16cfd492b3d87c58ba
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-16 21:54:06 +0300

    function to define a class Student.

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,19 +6,28 @@ function to define a class Student.
 
 class Student:
     """
-    student class module
+    Represents a student.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
     """
     def __init__(self, first_name, last_name, age):
-        """
-        instantation
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """
-        retrieves dictionary representation
+        Retrieves a dictionary representation of a Student instance.
+
+        params:
+            attrs (list): list of strings specifying which attributes to retrieve.
+                If None, all attributes will be retrieved.
+
+        Returns:
+            dict: Dictionary representation of the Student instance.
         """
         my_list = self.__dict__
         stringdict_ = dict()
@@ -37,7 +46,14 @@ class Student:
 
     def reload_from_json(self, json):
         """
-        replaces all attributes of the student instance
+        Replaces all attributes of the Student instance
+        based on a dictionary representation.
+
+        params:
+            json (dict): A dictionary containing attribute names and values.
+
+        Returns:
+            None
         """
         for i in json:
             if i in self.__dict__.keys():
