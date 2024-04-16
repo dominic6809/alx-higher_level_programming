b3070b826b1a2a7d62fe7ad702d7361eaf380184
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-16 22:19:12 +0300

    Reads from standard input and computes metrics.

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,46 +4,54 @@ Reads from standard input and computes metrics.
 """
 
 
-def print_stats(size, status_codes):
-    print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
+import sys
 
 
-if __name__ == "__main__":
-    import sys
+def print_info():
+    print('File size: {:d}'.format(file_size))
 
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
+    for scode, code_times in sorted(status_codes.items()):
+        if code_times > 0:
+            print('{}: {:d}'.format(scode, code_times))
 
-    try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
 
-            line = line.split()
+status_codes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
 
-            try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
+count = 0
+file_size = 0
 
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            print_info()
 
-        print_stats(size, status_codes)
+        pieces = line.split()
 
-    except KeyboardInterrupt:
-        print_stats(size, status_codes)
-        raise
+        try:
+            status = int(pieces[-2])
+
+            if str(status) in status_codes.keys():
+                status_codes[str(status)] += 1
+        except (ValueError, IndexError):
+            pass
+
+        try:
+            file_size += int(pieces[-1])
+        except (IndexError, ValueError):
+            pass
+
+        count += 1
+
+    print_info()
+except KeyboardInterrupt:
+    print_info()
+    raise
