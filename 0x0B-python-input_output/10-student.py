e7c6812812607e49824b78295a2cf7e4d16265b0
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-16 22:24:45 +0300

    function to define a class Student.

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -8,7 +8,6 @@ class Student:
     """
     Defines a student by first_name, last_name, and age.
     """
-
     def __init__(self, first_name, last_name, age):
         """
         Initializes a Student instance with first_name, last_name, and age.
@@ -23,12 +22,19 @@ class Student:
 
     def to_json(self, attrs=None):
         """
-        Retrieves a dictionary representation of a Student instance.
-        params:
-            attrs (list): A list of strings representing attribute names.
-        Returns:
-            dict: A dictionary containing specified attributes of the Student.
+        retrieves dictionary representation
         """
-        if attrs is not None and isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
-        return self.__dict__
+        my_list = self.__dict__
+        stringdict_ = dict()
+
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return my_list
+
+                if attr in my_list:
+                    stringdict_[attr] = my_list[attr]
+
+            return stringdict_
+
+        return stringdict_
