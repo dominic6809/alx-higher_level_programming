124932d2cc34680967885770a36b45a6f331c622
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-05-21 18:12:22 +0300

    temperature #

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,9 +1,9 @@
 -- Task 19: script that Display the top 3 cities' temperatures during
 -- July and August ordered by temperature (descending)
 
-SELECT city, MAX(value) AS avg_temp
-FROM temperatures
-WHERE MONTH(date) IN (7, 8)
-GROUP BY city
-ORDER BY avg_temp DESC
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
 LIMIT 3;
