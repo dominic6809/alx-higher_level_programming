8aa5a35087a59c9b3269a4a0c07992545a316420
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-05-21 18:13:49 +0300

    move to utf-8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,6 @@
 -- Task 17: Convert hbtn_0c_0 database, first_table table, and name field to UTF8
 
+USE `hbtn_0c_0`
 -- Convert hbtn_0c_0 database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Convert first_table table
