f0f6ed0822570fd4a6b700b262bfb903c74de0cf
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-05-22 19:12:31 +0300

    List all shows from hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,13 +1,6 @@
 -- Task 19: List all shows from hbtn_0d_tvshows_rate by their rating
 
-SELECT 
-    tv_shows.title, 
-    SUM(tv_show_ratings.rating) AS rating
-FROM 
-    tv_shows
-JOIN 
-    tv_show_ratings ON tv_shows.id = tv_show_ratings.tv_show_id
-GROUP BY 
-    tv_shows.title
-ORDER BY 
-    rating DESC;
+SELECT tv_shows.title, SUM(tv_show_ratings.rate)
+AS rating FROM tv_shows INNER JOIN tv_show_ratings
+ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title ORDER BY rating DESC;
