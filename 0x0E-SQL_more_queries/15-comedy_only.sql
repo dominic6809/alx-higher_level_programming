e429c133f81137a962af24df516e6b8921219083
dominic6809 <muuomusyoki018@gmail.com>
2024-05-22 08:22:26 -0700

    comedy only

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,14 @@
+-- Task 15: List all Comedy shows from hbtn_0d_tvshows
+
+SELECT 
+    tv_shows.title
+FROM 
+    tv_shows
+JOIN 
+    tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN 
+    tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE 
+    tv_genres.name = 'Comedy'
+ORDER BY 
+    tv_shows.title ASC;
