1701958194ebdf9d316e3e07b78f25d17910a88a
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-05-22 19:01:09 +0300

    my genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,14 +1,11 @@
 -- Task 14: List all genres of the show Dexter from hbtn_0d_tvshows
 
-SELECT 
+SELECT
     tv_genres.name
-FROM 
-    tv_shows
-JOIN 
-    tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN 
-    tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE 
-    tv_shows.title = 'Dexter'
-ORDER BY 
-    tv_genres.name ASC;
+    FROM
+    tv_genres
+    INNER JOIN
+    tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = "Dexter"
+    ORDER BY tv_genres.name;
