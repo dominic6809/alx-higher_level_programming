4bffc05366c6486deae76b59127cfd291e4690f7
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-05-22 18:59:01 +0300

    count shows

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,11 +1,6 @@
 -- Task 13: List all genres from hbtn_0d_tvshows with the number of shows linked to each
 
-SELECT 
-    tv_show_genres.genre_id AS genre,
-    COUNT(tv_show_genres.tv_show_id) AS number_of_shows
-FROM 
-    tv_show_genres
-GROUP BY 
-    tv_show_genres.genre_id
-ORDER BY 
-    number_of_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(show_id) AS number_of_shows
+FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
