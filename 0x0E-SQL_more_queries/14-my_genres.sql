d2380655f28ff419f2210c422f3859075f19fba2
dominic6809 <muuomusyoki018@gmail.com>
2024-05-22 08:21:40 -0700

    my genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,14 @@
+-- Task 14: List all genres of the show Dexter from hbtn_0d_tvshows
+
+SELECT 
+    tv_genres.name
+FROM 
+    tv_shows
+JOIN 
+    tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN 
+    tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE 
+    tv_shows.title = 'Dexter'
+ORDER BY 
+    tv_genres.name ASC;
