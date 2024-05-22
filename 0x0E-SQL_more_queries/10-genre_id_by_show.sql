f16042d5afda9d114348118bc0dc631885795a1c
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-05-22 18:56:25 +0300

    List all shows in the database hbtn_0d_tvshows with at least one linked genre

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,11 +1,5 @@
 -- Task 10: List all shows in the database hbtn_0d_tvshows with at least one linked genre
 
-SELECT 
-    tv_shows.title, 
-    tv_show_genres.genre_id
-FROM 
-    tv_shows
-JOIN 
-    tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY 
-    tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
