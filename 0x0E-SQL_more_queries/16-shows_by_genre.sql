358fe5a1cecdf391edda542000a1003fd72389d4
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-05-22 19:05:51 +0300

    show by genre

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,13 +1,10 @@
 -- Task 16: List all shows and their linked genres from hbtn_0d_tvshows
 
-SELECT 
-    tv_shows.title, 
+SELECT
+    tv_shows.title,
     tv_genres.name
-FROM 
+    FROM
     tv_shows
-LEFT JOIN 
-    tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-LEFT JOIN 
-    tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY 
-    tv_shows.title, tv_genres.name;
+    LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+    LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+    ORDER BY tv_shows.title, tv_genres.name;
