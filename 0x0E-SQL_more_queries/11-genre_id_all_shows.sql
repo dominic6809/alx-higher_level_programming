9b2caefabc0dcff596465bafa8fc784a3096dd82
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-05-22 18:57:29 +0300

    script that lists all shows contained in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,12 @@
 -- Task 11: List all shows in the database hbtn_0d_tvshows with their genre_id
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- script that lists all shows contained in the database hbtn_0d_tvshows.
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    tv_shows
+LEFT JOIN
+    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY
+    tv_shows.title, tv_show_genres.genre_id;
