7ae2a56c72e79bb5a0bf0a1489183aa7068cb298
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-17 13:51:24 +0300

    searches the second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,21 +3,21 @@
 /* Get the arguments passed to the script
 (excluding the first two which are node and script name)
 */
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 
 // Convert arguments to integers and filter out any NaN values
-const numbers = args.map(arg => parseInt(arg)).filter(num => !isNaN(num))
+const numbers = args.map(arg => parseInt(arg)).filter(num => !isNaN(num));
 
 if (numbers.length === 0) {
-  console.log(0)
+  console.log(0);
 } else if (numbers.length === 1) {
   // If there is only one valid number, print 0
-  console.log(0)
+  console.log(0);
 } else {
   // Sort numbers in descending order
-  numbers.sort((a, b) => b - a)
+  numbers.sort((a, b) => b - a);
 
-  const secondBiggest = numbers[1]
+  const secondBiggest = numbers[1];
 
-  console.log(secondBiggest)
+  console.log(secondBiggest);
 }
