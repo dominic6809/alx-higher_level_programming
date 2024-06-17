3115ddc99f94ace6577cd956a8c93a50cb991f07
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-17 13:53:29 +0300

    script to replace the value 12 with 89:

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -2,10 +2,10 @@
 const myObject = {
   type: 'object',
   value: 12
-}
-console.log(myObject)
+};
+console.log(myObject);
 
 // Update the value of the 'value' property to 89
-myObject.value = 89
+myObject.value = 89;
 
-console.log(myObject)
+console.log(myObject);
