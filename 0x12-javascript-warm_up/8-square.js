30d72197d56091f9b0d801a936d31193f547d91b
dominic6809 <muuomusyoki018@gmail.com>
2024-06-17 03:20:47 -0700

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
 // Get the first argument passed to the script
-const args = process.argv[2];
+const args = process.argv[2]
 
 // Convert the argument to an integer
-const sizeInt = parseInt(args);
+const sizeInt = parseInt(args)
 
 // Check if the conversion is successful and the size is a positive integer
 if (!isNaN(sizeInt) && sizeInt > 0) {
   for (let i = 0; i < sizeInt; i++) {
-    const line = 'X'.repeat(sizeInt);
-    console.log(line);
+    const line = 'X'.repeat(sizeInt)
+    console.log(line)
   }
 } else {
-  console.log("Missing size");
+  console.log('Missing size')
 }
