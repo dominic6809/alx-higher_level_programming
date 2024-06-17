5c31573b945a724464348b23bc83ed7387427636
dominic6809 <muuomusyoki018@gmail.com>
2024-06-17 02:06:42 -0700

    print 3 lines

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Create an array with the desired strings
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < messages.length; ++i) {
+    console.log(messages[i]);
+}
