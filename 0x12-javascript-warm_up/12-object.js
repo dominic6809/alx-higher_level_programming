c158e9b55d4884ef853cfb93c1802b89856ebd4f
dominic6809 <muuomusyoki018@gmail.com>
2024-06-17 02:41:22 -0700

    Update this script to replace the value 12 with 89:

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const myObject = {
+  type: "object",
+  value: 12,
+};
+console.log(myObject);
+
+// Update the value of the 'value' property to 89
+myObject.value = 89;
+
+console.log(myObject);
