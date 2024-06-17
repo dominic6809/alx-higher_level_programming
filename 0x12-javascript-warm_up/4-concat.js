8c7b1bddcb34020b2f71e5b46d692d759baa283e
dominic6809 <muuomusyoki018@gmail.com>
2024-06-17 03:18:37 -0700

    prints two arguments passed to it, in the following format: “ is ”

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Get the arguments passed to the script
-const [args1, args2] = process.argv.slice(2);
+const [args1, args2] = process.argv.slice(2)
 
 // Print the arguments in the specified format
-console.log(`${args1} is ${args2}`);
+console.log(`${args1} is ${args2}`)
