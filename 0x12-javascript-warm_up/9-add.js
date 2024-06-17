7b2a676007860e6dc75e895db6039f3c40a22db7
dominic6809 <muuomusyoki018@gmail.com>
2024-06-17 03:21:18 -0700

    prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,22 +1,22 @@
 #!/usr/bin/node
 
 // Define the add function
-function add(a, b) {
-  return a + b;
+function add (a, b) {
+  return a + b
 }
 
-const args1 = process.argv[2];
-const args2 = process.argv[3];
+const args1 = process.argv[2]
+const args2 = process.argv[3]
 
 // Convert the arguments to integers
-const num1 = parseInt(args1);
-const num2 = parseInt(args2);
+const num1 = parseInt(args1)
+const num2 = parseInt(args2)
 
 // Check if both arguments are valid integers
 if (!isNaN(num1) && !isNaN(num2)) {
   // Function call
-  const result = add(num1, num2);
-  console.log(result);
+  const result = add(num1, num2)
+  console.log(result)
 } else {
-  console.log("NaN");
+  console.log('NaN')
 }
