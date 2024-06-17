b1ec2e17b01ff8e38455fd508a9deac650679621
dominic6809 <muuomusyoki018@gmail.com>
2024-06-17 04:36:42 -0700

    Increment object

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// script by adding a new function incr that increments the integer value.
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
