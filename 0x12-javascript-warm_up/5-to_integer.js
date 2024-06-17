72e26e5842da60049e68848172419142a3384bbf
dominic6809 <muuomusyoki018@gmail.com>
2024-06-17 00:43:01 -0700

    executed files && an integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const args = process.argv[2];
+
+// Convert the argument to an integer
+const number = parseInt(args, 10);
+
+// Check if the conversion is successful
+if (!isNaN(number)) {
+  console.log(`My number: ${number}`);
+} else {
+  console.log("Not a number");
+}
