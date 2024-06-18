ee33a6605cc5748f3ac70aa2849b07d0badc0bf7
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-18 14:51:25 +0300

    sorted occurrences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,18 +1,22 @@
 #!/usr/bin/node
 
-/* script that imports a dictionary of occurrences by user id
+/*
+script that imports a dictionary of occurrences by user id
 and computes a dictionary of user ids by occurrence.
 */
-const { dict } = require('./101-data');
+const dict = require('./101-data').dict;
 
+const newlist = Object.entries(dict);
+const vals = Object.values(dict);
+const uniqVals = [...new Set(vals)];
 const newDict = {};
-
-for (const userId in dict) {
-  const occurrences = dict[userId];
-  if (!newDict[occurrences]) {
-    newDict[occurrences] = [];
+for (const j in uniqVals) {
+  const list = [];
+  for (const k in newlist) {
+    if (newlist[k][1] === uniqVals[j]) {
+      list.unshift(newlist[k][0]);
+    }
   }
-  newDict[occurrences].push(userId);
+  newDict[uniqVals[j]] = list;
 }
-
 console.log(newDict);
