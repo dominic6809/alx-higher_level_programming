822837488a912df092327c1a1a1dee5c47cc9e1d
dominic6809 <muuomusyoki018@gmail.com>
2024-06-18 03:39:29 -0700

    convert Number

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+ function that converts a number from base 10 to another base passed as argument:
+Prototype: exports.converter = function (base)
+*/
+exports.converter = function (base) {
+  return function (myNumber) {
+    return myNumber.toString(base);
+  }
+};
+
