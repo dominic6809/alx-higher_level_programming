4ce1b9c6be5b2047b3c893a0907c37e7ffa4ac60
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-18 14:26:00 +0300

    script that imports an array and computes a new arra

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
+
 /* script that imports an array and computes a new array. */
+const lst = require('./100-data').list;
 
-const { arr } = require('./100-data');
-
-const newList = arr.map((value, index) => value * index);
-
-console.log(arr);
-console.log(newList);
+console.log(lst);
+console.log(lst.map((x, idx) => x * idx));
 
