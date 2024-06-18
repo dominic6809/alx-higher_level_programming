8928ff8f8f987ee9b33adc5e4a13340af43144bb
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-18 13:24:42 +0300

    Rectangle that defines a rectangl

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -12,17 +12,19 @@ Create an instance method called print() that prints the rectangle using the cha
 */
 class Rectangle {
   constructor (w, h) {
-    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
-    this.width = w;
-    this.height = h;
+    if ((w > 0 && h > 0)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return {};
+    }
   }
 
-  // Instance method print() that prints the rectangle using the character X
   print () {
     if (!this.width || !this.height) {
-      console.log('Invalid dimensions for the rectangle.');
+      console.log('Invalid dimensions for rectangle.');
       return;
-    }
+
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
