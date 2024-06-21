9ac49f1f6181c55790ea031a653e1d2ad1e72a2e
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-21 21:44:07 +0300

    relationship_states_cities_list

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
 """
-prints the State object with the name passed as argument
-from the db
+Prints the State object with the name passed as argument
+from the database.
 """
 
 from relationship_city import City
 from relationship_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import relationship
 import sys
 
-
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        )
+    )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+
     for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
+        print(f"{instance.id}: {instance.name}")
         for city_ins in instance.cities:
-            print("    ", end="")
-            print(city_ins.id, city_ins.name, sep=": ")
+            print(f"    {city_ins.id}: {city_ins.name}")
+
+    session.close()
