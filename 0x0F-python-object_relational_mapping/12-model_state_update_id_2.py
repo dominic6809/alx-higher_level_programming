26e548a289944d8ed189685005f686c5fcd035b9
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-21 21:07:28 +0300

    update a state model

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 """
-script that prints the State object with the name passed as argument
-from the database
+Script that prints the State object with the name passed as argument
+from the database.
 """
+
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys
 
-
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        )
+    )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+    
     new_instance = session.query(State).filter_by(id=2).first()
-    new_instance.name = 'New Mexico'
-    session.commit()
+    if new_instance:
+        new_instance.name = 'New Mexico'
+        session.commit()
+
+    session.close()
