96bb5d4bcf539f056f31b5f9ff0080207f4ca72f
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-21 21:04:18 +0300

    get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,10 +10,11 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 
 if __name__ == "__main__":
-    """Access to the database and get a state"""
+    """Access the database and get a state."""
 
     db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
+        argv[1], argv[2], argv[3]
+    )
 
     engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
