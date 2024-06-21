fa30775bc357a9ae9890e41c7319fa10389f38c1
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-21 21:41:02 +0300

    city model

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """
-script that defines a City class
+Script that defines a City class
 to work with MySQLAlchemy ORM.
 """
-from model_state import Base, State
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class City(Base):
     """
-    City class module
-    Attribs:
-        __tablename__ (str): The table name of the class
-        id (int): The id of the class
-        name (str): The name of the class
-        state_id (int): The state the city belongs to
+    City class module.
+
+    Attributes:
+        __tablename__ (str): The table name of the class.
+        id (int): The id of the class.
+        name (str): The name of the class.
+        state_id (int): The state the city belongs to.
     """
 
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
