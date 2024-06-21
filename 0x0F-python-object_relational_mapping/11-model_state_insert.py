e1ac4f8de18421baf96b0110f8fef954959f2d46
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-21 14:30:24 +0300

    add state object

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+module that adds the State object
+`Louisiana` to the database `hbtn_0e_6_usa`.
+"""
+
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Access to the database and get a state
+    from db
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    n_state = State(name="Louisiana")
+    session.add(n_state)
+    session.commit()
+
+    print('{0}'.format(n_state.id))
+    session.close()
