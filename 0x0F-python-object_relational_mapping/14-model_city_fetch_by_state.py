7de369d46809ff8fd1767e07f333fc2139080960
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-21 21:12:15 +0300

    cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 """
-Class definition of a city
-prints all City objects from the database hbtn_0e_14_usa
+Script that prints all City objects from the database hbtn_0e_14_usa.
 """
+
 from model_state import Base, State
 from model_city import City
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys
 
-
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        )
+    )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in (session.query(State.name, City.id, City.name)
-                     .filter(State.id == City.state_id)):
-        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
+
+    for instance in session.query(State.name, City.id, City.name).filter(State.id == City.state_id):
+        print(f"{instance[0]}: ({instance[1]}) {instance[2]}")
+
+    session.close()
