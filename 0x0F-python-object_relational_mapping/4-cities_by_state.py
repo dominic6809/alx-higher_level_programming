bbf6a8b2ab60a97f3986fbe7d532267fc21bbb41
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-21 10:47:38 +0300

    list all cities from the db

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
 """
-it lists all cities from
-the database `hbtn_0e_4_usa`.
+lists all cities from the database hbtn_0e_4_usa
 """
 
-import MySQLdb as db
+import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
     """
-    Access the database and get the cities from the db
+    Access the database and get the cities
+    from the db
     """
 
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    with db_connect.cursor() as db_cursor:
-        cursor.execute("SELECT cities.id, cities.name, states.name \
-                                FROM cities JOIN states ON cities.state_id \
-                                = states.id ORDER BY cities.id ASC")
-        rows_selected = cursor.fetchall()
+    with db.cursor() as cur_ptr:
+        cur_ptr.execute("""
+            SELECT
+                cities.id, cities.name, states.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            ORDER BY
+                cities.id ASC
+        """)
 
-    if rows_selected is not None:
-        for row in rows_selected:
+        rows = cur_ptr.fetchall()
+
+    if rows is not None:
+        for row in rows:
             print(row)
