30e45d6af2fd1f4f8cfc7332ce0521f789755e5b
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-06-21 10:35:41 +0300

    filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """
-script that takes in an argument and
-displays all values in the states
-where `name` matches the argument
-from the database `hbtn_0e_0_usa`.
+script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
 """
 
-import MySQLdb as db
+import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    db_connect = db.connect(host="localhost", user=argv[1], port=3306,
-                            passwd=argv[2], db=argv[3])
-    cur_cursor = db_connect.cur_cursor()
+    """
+    Access the database and get the states
+    """
 
-    cur_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
-                        states.id ASC".format(argv[4]))
-    rows_selected = cur_cursor.fetchall()
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    for row in rows_selected:
+    ptr = db.cursor()
+    ptr.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
+    rows = ptr.fetchall()
+
+    for row in rows:
         print(row)
