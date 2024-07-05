f064b85147afaa93305720035bb58dae531140b3
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-07-05 14:18:52 +0300

    post an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {'email': email}
+
+    res = requests.post(url, data=data)
+    print(res.text)
