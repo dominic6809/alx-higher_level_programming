6489ca08deb360296a3571b960dcd0ffded20330
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-07-09 06:25:51 +0300

    curl requests to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # Send a GET request to the URL and save the response header and body
-curl -sL -w "%{http_code}" "$1" -o ./output -D ./header
-
-# Check if the HTTP status code is 200 OK
-if grep -q "200 OK" ./header; then
-    # Display the body of the response
-    cat ./output
-fi
+curl -sL -w "$1"
