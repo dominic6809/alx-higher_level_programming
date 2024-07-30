fa20110e24f0351bbdcb864588cf977a6bae8324
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-07-30 22:09:39 +0300

    completed_tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+/**
+ * Script that computes the number of tasks completed by user ID.
+ * The first argument is the API URL: https://jsonplaceholder.typicode.com/todos
+ * Only print users with completed tasks.
+ * Module used: request
+ */
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const todos = JSON.parse(body);
+    const completedTasksByUser = {};
+
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (completedTasksByUser[todo.userId]) {
+          completedTasksByUser[todo.userId]++;
+        } else {
+          completedTasksByUser[todo.userId] = 1;
+        }
+      }
+    });
+
+    console.log(completedTasksByUser);
+  }
+});
