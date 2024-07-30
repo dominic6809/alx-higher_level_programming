9cb165e05764d7c31f36a65af3ec91b8bc7f4a19
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-07-30 22:38:52 +0300

    -readme.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,20 +1,10 @@
 #!/usr/bin/node
-// reads and prints contents of a file
 
 const fs = require('fs');
-
-// getting the filepath from command line arguments
-const filepath = process.argv[2];
-
-// funtion to read the file
-function readFile (path) {
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(data);
-    }
-  });
-}
-// calling the function with the filepath
-readFile(filepath);
+fs.readFile(process.argv[2], 'utf8', function (error, data) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data.toString());
+  }
+});
