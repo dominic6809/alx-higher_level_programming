1cbdf0c26ebc48e353e33ab7acbcfcfa70522d87
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-08-07 22:41:25 +0300

    update text color

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var header = document.querySelector("header");
+
+  // Update the text color of the <header> element to red
+  if (header) {
+    header.style.color = "#FF0000";
+  }
+});
